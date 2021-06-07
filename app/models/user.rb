c9850a85215c39edb778294560799a4e73a0436f
korethisyo <korethis0816@gmail.com>
2021-06-07 04:52:48 +0000

    [Update]user_function

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   #validates :name, presense: true
   #validates :nick_name, presense: true
 
-  enum sex: {nil:0, 女性:1, 男性:2, その他:3, 無回答:4}
+  enum sex: {選択してください:0, 女性:1, 男性:2, その他:3, 無回答:4}
   attachment :user_image
 end
