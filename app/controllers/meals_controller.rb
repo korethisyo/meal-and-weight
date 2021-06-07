9664978a28017e6c0e0c5f6a28edad5e1b1f2b80
korethisyo <korethis0816@gmail.com>
2021-06-07 07:19:35 +0000

    [Update]meal_function

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,8 +1,14 @@
 class MealsController < ApplicationController
   def new
+    @meal = Meal.new
   end
 
   def create
+    @meal = Meal.new(meal_params)
+    @meal.user_id = current_user.id
+    @meal.save
+    binding.pry
+    redirect_to root_path
   end
 
   def show
@@ -12,5 +18,11 @@ class MealsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:date, :menu_image, :menu_detail)
   end
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,4 @@
 class Meal < ApplicationRecord
+  belongs_to :user
+  attachment :menu_image
 end
