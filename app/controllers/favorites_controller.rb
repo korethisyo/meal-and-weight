508a26a3b79dd49468ebc3bd085a10462707e199
korethisyo <korethis0816@gmail.com>
2021-06-10 23:43:57 +0000

    [Update]favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,19 @@
 class FavoritesController < ApplicationController
+  before_action :set_meal
+
   def create
-    @meal = Meal.find(params[:meal_id])
     @favorite = current_user.favorites.new(meal_id: @meal.id)
     @favorite.save
-    redirect_to request.referer
   end
 
   def destroy
-    @meal = Meal.find(params[:meal_id])
     @favorite = current_user.favorites.find_by(meal_id: @meal.id)
     @favorite.destroy
-    redirect_to request.referer
+  end
+
+  private
+  #上記で使用する@mealの値を先に取得
+  def set_meal
+    @meal = Meal.find(params[:meal_id])
   end
 end
