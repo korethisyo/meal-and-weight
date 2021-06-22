09baca53f0cb517a22b9b7fa5509e39b501e6ae2
korethisyo <korethis0816@gmail.com>
2021-06-22 08:17:22 +0000

    [Update]test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,1 +1,26 @@
 require 'rails_helper'
+
+describe 'Userモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    describe '新規登録の時' do
+      it "氏名、ニックネーム、メールアドレス、パスワードがある場合有効" do
+        user = FactoryBot.build(:user)
+        expect(user).to be_valid
+      end
+      # it "氏名がない場合無効" do
+      # end
+      # it "ニックネームがない場合無効" do
+      # end
+      # it "メールアドレスがない場合無効" do
+      # end
+      # it "パスワードがない場合無効" do
+      # end
+    end
+
+    # describe 'ログインの時' do
+    #   it "メールアドレス、パスワードがある場合有効"
+    #   it "メールアドレスがない場合無効"
+    #   it "パスワードがない場合無効"
+    # end
+  end
+end
