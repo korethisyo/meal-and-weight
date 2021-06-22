3de8670538d05e47d6365d14e4edfa2fa0798f29
korethisyo <korethis0816@gmail.com>
2021-06-22 10:08:04 +0000

    [Update]test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,25 +2,33 @@ require 'rails_helper'
 
 describe 'Userモデルのテスト', type: :model do
   describe 'バリデーションのテスト' do
-    describe '新規登録の時' do
-      it "氏名、ニックネーム、メールアドレス、パスワードがある場合有効" do
-        user = FactoryBot.build(:user)
-        expect(user).to be_valid
-      end
-      # it "氏名がない場合無効" do
-      # end
-      # it "ニックネームがない場合無効" do
-      # end
-      # it "メールアドレスがない場合無効" do
-      # end
-      # it "パスワードがない場合無効" do
-      # end
+    it "氏名、ニックネーム、メールアドレス、パスワードがある場合有効" do
+      user = FactoryBot.build(:user)
+      expect(user).to be_valid
     end
 
-    # describe 'ログインの時' do
-    #   it "メールアドレス、パスワードがある場合有効"
-    #   it "メールアドレスがない場合無効"
-    #   it "パスワードがない場合無効"
-    # end
+    it "氏名がない場合無効" do
+      user = FactoryBot.build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+
+    it "ニックネームがない場合無効" do
+      user = FactoryBot.build(:user, nick_name: nil)
+      user.valid?
+      expect(user.errors[:nick_name]).to include("can't be blank")
+    end
+
+    it "メールアドレスがない場合無効" do
+      user = FactoryBot.build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "パスワードがない場合無効" do
+      user = FactoryBot.build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
   end
 end
